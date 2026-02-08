c9d381b81cfe7fd062b01c02afd27766e1690025
durga-mahesh-333 <67140575+durga-mahesh-333@users.noreply.github.com>
2026-02-08 07:22:59 +0000

    recursion: fibanocci

---
+++ recursion/fibonacci_number.py
@@ -0,0 +1,12 @@
+def fib( n):
+    """
+    :type n: int
+    :rtype: int
+    """
+    if n==1:
+        return 1
+    elif n<=0:
+        return 0
+    else :
+        return fib(n-1)+fib(n-2)
+print(fib(2))
