4268b1c7109dbc4b0152ad6cfd1f8d0faccf3728
durga-mahesh-333 <67140575+durga-mahesh-333@users.noreply.github.com>
2026-03-14 19:28:47 +0000

    recursion practice : power_x_n pending III

--- recursion/pow_x_n.py
+++ recursion/pow_x_n.py
@@ -3,7 +3,7 @@ def myPow( x: float, n: int) -> float:
         def helper(given_val:float , pow_val: float , exp:int, extra_exp:int , helper_exp:int , recur:int):
             if exp==1: return pow_val,extra_exp
             if exp%2==1:
-                if recur==0:extra_exp=helper_exp
+                extra_exp=1
                 extra_exp*=helper_exp
                 exp-=1
             
@@ -12,11 +12,11 @@ def myPow( x: float, n: int) -> float:
             
             return helper(given_val , pow_val , exp,extra_exp , helper_exp*2, recur+1)
 
-        return helper(x,x,n,0,1,0)
-
-        # def helper_ext_pow(given_val,pow_val,ext_pow):
-        #     if ext_pow==0:
-        #         return pow_val
-        #     return helper_ext_pow(given_val , pow_val*given_val , ext_pow-1) 
-        # return helper_ext_pow(x,pow_val,ext_pow)
-print(myPow(2.0 , 6))
+        pow_val,ext_pow = helper(x,x,n,0,1,0)
+        return  pow_val,ext_pow
+        def helper_ext_pow(given_val,pow_val,ext_pow):
+            if ext_pow==0:
+                return pow_val
+            return helper_ext_pow(given_val , pow_val*given_val , ext_pow-1) 
+        return helper_ext_pow(x,pow_val,ext_pow)
+print(myPow(2,13))
