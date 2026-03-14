84ab32cfc92e505796681ca16e8a7e867a60a206
durga-mahesh-333 <67140575+durga-mahesh-333@users.noreply.github.com>
2026-03-14 19:52:58 +0000

    recursion practice : power_x_n pending IV : positive exp done

--- recursion/pow_x_n.py
+++ recursion/pow_x_n.py
@@ -1,22 +1,37 @@
 def myPow( x: float, n: int) -> float:
-       
+        if x==0: return 0
         def helper(given_val:float , pow_val: float , exp:int, extra_exp:int , helper_exp:int , recur:int):
-            if exp==1: return pow_val,extra_exp
-            if exp%2==1:
-                extra_exp=1
-                extra_exp*=helper_exp
-                exp-=1
-            
-            exp//=2
-            pow_val*=pow_val
-            
-            return helper(given_val , pow_val , exp,extra_exp , helper_exp*2, recur+1)
+            if exp>0:
+                if exp==1: return pow_val,extra_exp
+                if exp%2==1:
+                    extra_exp+=helper_exp
+                    exp-=1
+                
+                exp//=2
+                pow_val*=pow_val
+                
+                return helper(given_val , pow_val , exp,extra_exp , helper_exp*2, recur+1)
+            else:
+                if exp==-1: return 1/pow_val,extra_exp
+                if exp%2==-1:
+                    extra_exp-=helper_exp
+                    exp+=1
+                
+                exp//=2
+                pow_val=1/(pow_val*pow_val)
+                
+                return helper(given_val , pow_val , exp,extra_exp , helper_exp*2, recur+1)
 
         pow_val,ext_pow = helper(x,x,n,0,1,0)
-        return  pow_val,ext_pow
+
         def helper_ext_pow(given_val,pow_val,ext_pow):
-            if ext_pow==0:
-                return pow_val
-            return helper_ext_pow(given_val , pow_val*given_val , ext_pow-1) 
+            if ext_pow>0:
+                if ext_pow==0:
+                    return pow_val
+                return helper_ext_pow(given_val , pow_val*given_val , ext_pow-1) 
+            else:
+                if ext_pow==0:
+                    return pow_val
+                return helper_ext_pow(given_val , pow_val*given_val , ext_pow+1) 
         return helper_ext_pow(x,pow_val,ext_pow)
-print(myPow(2,13))
+print(myPow(-1,35))
