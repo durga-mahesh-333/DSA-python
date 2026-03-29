d8df63b54a829bb087c0c217d30695a564d2286d
durga-mahesh-333 <67140575+durga-mahesh-333@users.noreply.github.com>
2026-03-29 05:18:36 +0000

    recursion practice : pow x n finished

--- recursion/pow_x_n.py
+++ recursion/pow_x_n.py
@@ -1,15 +1,18 @@
 import sys
 sys.set_int_max_str_digits(1000000000)
 def myPow( x: float, n: int) -> float:
-    def helper(updt_pow:int , extra_pow:int , powered_val:float ):
-        if updt_pow==1:
-            return powered_val*powered_val if extra_pow==0 else  powered_val*powered_val*powered_val
-        if updt_pow%2==1:
-            updt_pow-=1
-            extra_pow=1
-        updt_powered_val= helper(updt_pow=updt_pow//2,extra_pow=extra_pow , powered_val=powered_val)
-        return helper(updt_pow=updt_pow//2,extra_pow=extra_pow , powered_val=updt_powered_val)
+    if n==0: return 1
+    if n==1: return x
+    if abs(x)==1 and n<0 :
+        return abs(x) if n%2==0 else x
+    def helper(req_val:float, expo:int):
+        if expo==0:
+            return 1
+        updt_val=helper(req_val, expo//2)
+        return updt_val*updt_val if expo%2==0 else updt_val*updt_val*req_val
     
-    return helper(n,0,x)
+    final_val= helper(x,abs(n))
+    return final_val if n>0 else 1/final_val
+
         
-print(myPow(2,11))
+print(myPow(-1,-2147483648))
