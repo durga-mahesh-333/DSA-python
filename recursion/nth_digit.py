2ee663cbff0f952aae97e5e52e6bf7c83128f5ac
durga-mahesh-333 <67140575+durga-mahesh-333@users.noreply.github.com>
2026-06-10 18:39:40 +0000

    recursion practice: nth digit , status : completed

--- recursion/nth_digit.py
+++ recursion/nth_digit.py
@@ -12,16 +12,20 @@ def findNthDigit(n: int) -> int:
         return recur_helper(no_of_digits+1,current_incr_value)
     max_digit_count_of_prev_digit_count , digit_count_for_each_num  = recur_helper(1,  9 )
 
-    num_of_values_from_max_digit_count_of_prev_digit_count= (n-max_digit_count_of_prev_digit_count)//digit_count_for_each_num
-
     prev_digit_count= digit_count_for_each_num-1
     max_num_for_prev_digit_count= 10**prev_digit_count-1
-    value_to_check_for_nth_digit=max_num_for_prev_digit_count + num_of_values_from_max_digit_count_of_prev_digit_count
+    no_of_digits_in_curr_digit_count = n-max_digit_count_of_prev_digit_count
+    final_num_with_leftover_digits = max_num_for_prev_digit_count+ \
+                                            (no_of_digits_in_curr_digit_count//digit_count_for_each_num) 
+    left_over_digits = no_of_digits_in_curr_digit_count%digit_count_for_each_num
 
-    return max_digit_count_of_prev_digit_count
+    final_numer_to_check=final_num_with_leftover_digits + (1 if left_over_digits else 0)
+
+    return int(str(final_numer_to_check)[left_over_digits-1])
 
 
-print(findNthDigit(191))
+
+print(findNthDigit(146))
 
 
 # prev_incr_value+(prev_incr_value*(10**ten_pow)*2)
